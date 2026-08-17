360f3873f28578c6ef337708e11abfc99f3d21f8
cvsubacoormis-cvsueduph <cvsubacoor.mis@cvsu.edu.ph>
2026-08-17 10:16:15 +0800

    Remove empty news item from newsData
    Removed empty news item from newsData array.

--- data/news.ts
+++ data/news.ts
@@ -1,15 +1,3 @@
 export const newsData = [
-  {
-    id: "1",
-    title: "",
-    date: "",
-    author: "",
-    category: "",
-    image: "",
-    excerpt:
-      "",
-    content:
-      "",
-    link: "",
-  },
+
 ];
